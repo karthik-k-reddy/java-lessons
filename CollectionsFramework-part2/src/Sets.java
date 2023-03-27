38ac374174c8c25313cd55e8dfd31498fcc39347
Karthik.K <karthik.k@arvindinternet.com>
2023-03-27 07:41:31 +0530

    LinkedHashSet,HashSet,TreeSet

--- CollectionsFramework-part2/src/Sets.java
+++ CollectionsFramework-part2/src/Sets.java
@@ -1,5 +1,35 @@
-public class Main {
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Set<Object> set=new HashSet<>();
+        addEleToSet(set);
+         printSet(set);
+        System.out.println("****************************");
+
+        Set<Object> set1=new LinkedHashSet<>();
+        addEleToSet(set1);
+        printSet(set1);
+        System.out.println("****************************");
+
+        Set<Object> set2=new TreeSet<>();
+        addEleToSet(set2);
+        printSet(set2);
+        System.out.println("****************************");
+}
+
+static void addEleToSet(Set<Object> s){
+    for(int i=0;i<21;i++){
+        s.add("A"+i);
     }
+}
+static void printSet(Set<?> s){
+        for(Object o:s){
+            System.out.println(o);
+        }
+}
 }
